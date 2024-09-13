734d5c8184172e19e00e636c93cf79cef6857a8d
Ankita Sagar <ankitasagar6265@gmail.com>
2024-09-13 21:58:04 +0530

    Add files via upload
    second commit

---
+++ AdultOrChild.java
@@ -0,0 +1,18 @@
+
+ public class AdultOrChild {
+    public static void main(String[] args) {
+        // Assign age
+        int age = 20;
+         //using if statement to check if adult or child
+         if(age >= 18) 
+         {
+            System.out.println("The person is an adult.");
+         }
+         if(age < 18)
+         {
+            System.out.println("The person is a child.");
+         }
+    }
+}
+    
+
